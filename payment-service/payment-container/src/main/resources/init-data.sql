1a1aaa8c624a269b055b5b35b48971825d3846cf
Adet <adet@Adetias-MacBook-Pro.local>
2025-01-28 13:34:06 +0700

    Implementing Container module

---
+++ payment-service/payment-container/src/main/resources/init-data.sql
@@ -0,0 +1,14 @@
+INSERT INTO payment.credit_entry(id, customer_id, total_credit_amount)
+    VALUES ('0194ab98-8d36-7f85-8767-629e74554ceb', '0191f8b6-3d9a-7d39-826b-4a75cc10a648', 500.00);
+INSERT INTO payment.credit_history(id, customer_id, amount, type)
+    VALUES ('0194ab98-8d36-7dcb-9c4c-bcd08f9391f4', '0191f8b6-3d9a-7d39-826b-4a75cc10a648', 100.00, 'CREDIT');
+INSERT INTO payment.credit_history(id, customer_id, amount, type)
+    VALUES ('0194ab98-8d36-7fe9-9d22-53963098d06b', '0191f8b6-3d9a-7d39-826b-4a75cc10a648', 600.00, 'CREDIT');
+INSERT INTO payment.credit_history(id, customer_id, amount, type)
+    VALUES ('0194ab98-8d36-7c66-80f3-144bbbe12b3c', '0191f8b6-3d9a-7d39-826b-4a75cc10a648', 200.00, 'DEBIT');
+
+
+INSERT INTO payment.credit_entry(id, customer_id, total_credit_amount)
+    VALUES ('0194ab98-8d36-736b-a24d-9aaf12d641ca', '0194ab98-8d36-7c54-b212-d44284df5306', 100.00);
+INSERT INTO payment.credit_history(id, customer_id, amount, type)
+    VALUES ('0194ab98-8d36-7083-9870-847de99d84b5', '0194ab98-8d36-7c54-b212-d44284df5306', 100.00, 'CREDIT');
