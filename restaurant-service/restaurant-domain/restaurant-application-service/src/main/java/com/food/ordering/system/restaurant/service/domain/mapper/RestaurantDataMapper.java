78fc05baae8e12c1b6be5cff03e1e5fb8d6ea8e1
Adet <>
2025-11-21 16:34:52 +0700

    Application service domain module: Implementing input ports

--- restaurant-service/restaurant-domain/restaurant-application-service/src/main/java/com/food/ordering/system/restaurant/service/domain/mapper/RestaurantDataMapper.java
+++ restaurant-service/restaurant-domain/restaurant-application-service/src/main/java/com/food/ordering/system/restaurant/service/domain/mapper/RestaurantDataMapper.java
@@ -1,7 +1,42 @@
 package com.food.ordering.system.restaurant.service.domain.mapper;
 
+import com.food.ordering.system.domain.valueobject.Money;
+import com.food.ordering.system.domain.valueobject.OrderId;
+import com.food.ordering.system.domain.valueobject.OrderStatus;
+import com.food.ordering.system.domain.valueobject.RestaurantId;
+import com.food.ordering.system.restaurant.service.domain.dto.RestaurantApprovalRequest;
+import com.food.ordering.system.restaurant.service.domain.entity.OrderDetail;
+import com.food.ordering.system.restaurant.service.domain.entity.Product;
+import com.food.ordering.system.restaurant.service.domain.entity.Restaurant;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.UUID;
+import java.util.stream.Collectors;
 
 @Component
 public class RestaurantDataMapper {
+
+    public Restaurant restaurantApprovalRequestToRestaurant(RestaurantApprovalRequest restaurantApprovalRequest) {
+
+        List<Product> productList = restaurantApprovalRequest.getProducts()
+                .stream()
+                .map(product -> Product.builder()
+                        .productId(product.getId())
+                        .quantity(product.getQuantity())
+                        .build())
+                .collect(Collectors.toList());
+
+        OrderDetail orderDetail = OrderDetail.builder()
+                .orderId(new OrderId(UUID.fromString(restaurantApprovalRequest.getOrderId())))
+                .products(productList)
+                .totalAmount(new Money(restaurantApprovalRequest.getPrice()))
+                .orderStatus(OrderStatus.valueOf(restaurantApprovalRequest.getRestaurantOrderStatus().name()))
+                .build();
+
+        return Restaurant.builder()
+                .restaurantId(new RestaurantId(UUID.fromString(restaurantApprovalRequest.getRestaurantId())))
+                .orderDetail(orderDetail)
+                .build();
+    }
 }
