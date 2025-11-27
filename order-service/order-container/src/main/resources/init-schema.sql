5e3c2d164297fb5ab34af1d6b8f41dee3aa76487
Adet <>
2025-11-27 10:59:37 +0700

    Updating order service database schema and config for outbox pattern

--- order-service/order-container/src/main/resources/init-schema.sql
+++ order-service/order-container/src/main/resources/init-schema.sql
@@ -5,7 +5,7 @@ CREATE SCHEMA "order";
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
 DROP TYPE IF EXISTS order_status;
-CREATE TYPE order_status AS ENUM ('PENDING', 'PAID', 'APPROVED', 'CANCELLING', 'CANCELLED');
+CREATE TYPE order_status AS ENUM ('PENDING', 'PAID', 'APPROVED', 'CANCELLED', 'CANCELLING');
 
 DROP TABLE IF EXISTS "order".orders CASCADE;
 
@@ -18,7 +18,7 @@ CREATE TABLE "order".orders
     price numeric(10,2) NOT NULL,
     order_status order_status NOT NULL,
     failure_messages character varying COLLATE pg_catalog."default",
-    CONSTRAINT order_pkey PRIMARY KEY (id)
+    CONSTRAINT orders_pkey PRIMARY KEY (id)
 );
 
 DROP TABLE IF EXISTS "order".order_items CASCADE;
@@ -59,3 +59,60 @@ ALTER TABLE "order".order_address
     ON UPDATE NO ACTION
     ON DELETE CASCADE
     NOT VALID;
+
+DROP TYPE IF EXISTS saga_status;
+CREATE TYPE saga_status AS ENUM ('STARTED', 'FAILED', 'SUCCEEDED', 'PROCESSING', 'COMPENSATING', 'COMPENSATED');
+
+DROP TYPE IF EXISTS outbox_status;
+CREATE TYPE outbox_status AS ENUM ('STARTED', 'COMPLETED', 'FAILED');
+
+DROP TABLE IF EXISTS "order".payment_outbox CASCADE;
+
+CREATE TABLE "order".payment_outbox
+(
+    id uuid NOT NULL,
+    saga_id uuid NOT NULL,
+    created_at TIMESTAMP WITH TIME ZONE NOT NULL,
+    processed_at TIMESTAMP WITH TIME ZONE,
+    type character varying COLLATE pg_catalog."default" NOT NULL,
+    payload jsonb NOT NULL,
+    outbox_status outbox_status NOT NULL,
+    saga_status saga_status NOT NULL,
+    order_status order_status NOT NULL,
+    version integer NOT NULL,
+    CONSTRAINT payment_outbox_pkey PRIMARY KEY (id)
+);
+
+CREATE INDEX "payment_outbox_saga_status"
+    ON "order".payment_outbox
+    (type, outbox_status, saga_status);
+
+--CREATE UNIQUE INDEX "payment_outbox_saga_id"
+--    ON "order".payment_outbox
+--    (type, saga_id, saga_status);
+
+DROP TABLE IF EXISTS "order".restaurant_approval_outbox CASCADE;
+
+CREATE TABLE "order".restaurant_approval_outbox
+(
+    id uuid NOT NULL,
+    saga_id uuid NOT NULL,
+    created_at TIMESTAMP WITH TIME ZONE NOT NULL,
+    processed_at TIMESTAMP WITH TIME ZONE,
+    type character varying COLLATE pg_catalog."default" NOT NULL,
+    payload jsonb NOT NULL,
+    outbox_status outbox_status NOT NULL,
+    saga_status saga_status NOT NULL,
+    order_status order_status NOT NULL,
+    version integer NOT NULL,
+    CONSTRAINT restaurant_approval_outbox_pkey PRIMARY KEY (id)
+);
+
+CREATE INDEX "restaurant_approval_outbox_saga_status"
+    ON "order".restaurant_approval_outbox
+    (type, outbox_status, saga_status);
+
+--CREATE UNIQUE INDEX "restaurant_approval_outbox_saga_id"
+--    ON "order".restaurant_approval_outbox
+--    (type, saga_id, saga_status);
+
