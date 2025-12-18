05edcfed43397f61d2810123db03fdd4a4008501
Adet <>
2025-12-18 15:36:44 +0700

    Testing payment request message listener for double payment

--- payment-service/payment-container/src/main/resources/init-data.sql
+++ payment-service/payment-container/src/main/resources/init-data.sql
@@ -6,9 +6,21 @@ INSERT INTO payment.credit_history(id, customer_id, amount, type)
     VALUES ('0194ab98-8d36-7fe9-9d22-53963098d06b', '0191f8b6-3d9a-7d39-826b-4a75cc10a648', 600.00, 'CREDIT');
 INSERT INTO payment.credit_history(id, customer_id, amount, type)
     VALUES ('0194ab98-8d36-7c66-80f3-144bbbe12b3c', '0191f8b6-3d9a-7d39-826b-4a75cc10a648', 200.00, 'DEBIT');
+INSERT INTO payment.credit_entry(id, customer_id, total_credit_amount)
+	VALUES ('d215b5f8-0249-4dc5-89a3-51fd148cfb21', 'd215b5f8-0249-4dc5-89a3-51fd148cfb41', 500.00);
+INSERT INTO payment.credit_history(id, customer_id, amount, type)
+	VALUES ('d215b5f8-0249-4dc5-89a3-51fd148cfb23', 'd215b5f8-0249-4dc5-89a3-51fd148cfb41', 100.00, 'CREDIT');
+INSERT INTO payment.credit_history(id, customer_id, amount, type)
+	VALUES ('d215b5f8-0249-4dc5-89a3-51fd148cfb24', 'd215b5f8-0249-4dc5-89a3-51fd148cfb41', 600.00, 'CREDIT');
+INSERT INTO payment.credit_history(id, customer_id, amount, type)
+	VALUES ('d215b5f8-0249-4dc5-89a3-51fd148cfb25', 'd215b5f8-0249-4dc5-89a3-51fd148cfb41', 200.00, 'DEBIT');
 
 
 INSERT INTO payment.credit_entry(id, customer_id, total_credit_amount)
     VALUES ('0194ab98-8d36-736b-a24d-9aaf12d641ca', '0194ab98-8d36-7c54-b212-d44284df5306', 100.00);
 INSERT INTO payment.credit_history(id, customer_id, amount, type)
     VALUES ('0194ab98-8d36-7083-9870-847de99d84b5', '0194ab98-8d36-7c54-b212-d44284df5306', 100.00, 'CREDIT');
+INSERT INTO payment.credit_entry(id, customer_id, total_credit_amount)
+	VALUES ('d215b5f8-0249-4dc5-89a3-51fd148cfb22', 'd215b5f8-0249-4dc5-89a3-51fd148cfb43', 100.00);
+INSERT INTO payment.credit_history(id, customer_id, amount, type)
+	VALUES ('d215b5f8-0249-4dc5-89a3-51fd148cfb26', 'd215b5f8-0249-4dc5-89a3-51fd148cfb43', 100.00, 'CREDIT');
