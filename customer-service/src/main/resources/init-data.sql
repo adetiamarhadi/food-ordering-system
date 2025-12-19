8d36846c710ca940a89a8a2b724a81bdb2663f0a
Adet <>
2025-12-19 11:00:25 +0700

    Testing the application end-to-end with outbox pattern changes

--- customer-service/src/main/resources/init-data.sql
+++ customer-service/src/main/resources/init-data.sql
@@ -1,5 +1,5 @@
 INSERT INTO customer.customers(id, username, first_name, last_name)
-    VALUES ('0191f8b6-3d9a-7d39-826b-4a75cc10a648', 'user_1', 'First', 'User');
+	VALUES ('d215b5f8-0249-4dc5-89a3-51fd148cfb41', 'user_1', 'First', 'User');
 
 INSERT INTO customer.customers(id, username, first_name, last_name)
-    VALUES ('0191f8b6-3d9a-76ef-8d1e-74e75ee25df4', 'user_2', 'Second', 'User');
+	VALUES ('d215b5f8-0249-4dc5-89a3-51fd148cfb42', 'user_2', 'Second', 'User');
